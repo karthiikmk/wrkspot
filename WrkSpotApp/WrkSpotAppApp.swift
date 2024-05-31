7a26df288e10b63a962c34bb222dfbc101b5ec94
Karthik <karthik.samy@anywhere.co>
2024-05-31 20:20:38 +0530

    Initial Commit

---
+++ WrkSpotApp/WrkSpotAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WrkSpotAppApp.swift
+//  WrkSpotApp
+//
+//  Created by Karthik on 31/05/24.
+//
+
+import SwiftUI
+
+@main
+struct WrkSpotAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
